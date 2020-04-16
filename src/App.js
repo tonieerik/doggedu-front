5f077907a36ed61fd9fe78c29014977932ae9fe2
Toni Leppänen <toni.leppanen@gmail.com>
2020-04-16 17:25:25 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Breadcrumb from "./components/Breadcrumb";
+import Category from "./components/Category";
+import Home from "./components/Home";
+import Settings from "./components/Settings";
+import Skill from "./components/Skill";
+import { useOvermind } from "./overmind";
+import { useAuth0 } from "./react-auth0-spa";
+import {
+  VIEW_HOME,
+  VIEW_CATEGORY,
+  VIEW_SKILL,
+  VIEW_SETTINGS
+} from "./utils/const";
+
+import "./App.css";
 
 function App() {
+  const { isAuthenticated, loginWithRedirect } = useAuth0();
+  const { state } = useOvermind();
+
+  if (!isAuthenticated) {
+    return (
+      <div className="App">
+        <button className="login" onClick={() => loginWithRedirect({})}>
+          KIRJAUDU SISÄÄN
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Breadcrumb />
+      {state.view === VIEW_CATEGORY && <Category />}
+      {state.view === VIEW_HOME && <Home />}
+      {state.view === VIEW_SKILL && <Skill />}
+      {state.view === VIEW_SETTINGS && <Settings />}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createOvermind } from "overmind";
+import { Provider } from "overmind-react";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { Auth0Provider } from "./react-auth0-spa";
+import auth_config from "./auth_config.json";
+import { config } from "./overmind";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./index.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const app = createOvermind(config, {
+  devtools: true
+});
+
+// A function that routes the user to the right place after login
+const onRedirectCallback = appState => {
+  window.history.replaceState(
+    {},
+    document.title,
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname
+  );
+};
+
+ReactDOM.render(
+  <Provider value={app}>
+    <Auth0Provider
+      domain={auth_config.domain}
+      client_id={auth_config.clientId}
+      redirect_uri={window.location.origin}
+      audience={auth_config.audience}
+      onRedirectCallback={onRedirectCallback}
+    >
+      <App />
+    </Auth0Provider>
+  </Provider>,
+  document.getElementById("root")
+);
+
 serviceWorker.unregister();
